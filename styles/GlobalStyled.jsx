23a7e126bef6217d3ebc6af70da864bbf9789d6d
Noon_sae <minseock7308@gmail.com>
2024-11-27 02:01:54 +0900

    💄 전역스타일에 reset-css 적용 및 normalizing

--- styles/GlobalStyled.jsx
+++ styles/GlobalStyled.jsx
@@ -1,2 +1,16 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
+
+export const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  * {
+    box-sizing: border-box;
+    font-size: 10px;
+    color: #111;
+  }
+
+  
+
+
+`;
