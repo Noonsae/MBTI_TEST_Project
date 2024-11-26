855fdf81b62862da725732efe5ed1199831058ce
Noon_sae <minseock7308@gmail.com>
2024-11-26 20:34:47 +0900

    🔗라우터 설정 및 연결

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,16 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
-import Dex from "../pages/Dex";
-import Details from "../pages/Details";
+import Signup from "../pages/Signup";
+import Profile from "../pages/Profile";
+import Test from "../pages/Test";
+import TestResult from "../pages/TestResult";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Routs */}
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/detail/:id" element={<Details />} />
+        <Route path="/" element={<Layout />}>
+          {/* Routs */}
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/test" element={<Test />} />
+          <Route path="/result" element={<TestResult />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
