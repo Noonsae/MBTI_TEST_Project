f2dbfed89a95408e584bc7564d4bb3b3f7f4158b
Noon_sae <minseock7308@gmail.com>
2024-11-27 07:03:05 +0900

    ✨Login 컴포넌트 생성 및 로직 작성

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,52 +1,83 @@
-import React, { useState } from "react";
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { login } from "../api/auth";
+import AuthForm from "../components/AuthForm";
+import { MyContext } from "../context/MyContext";
+import styled from "styled-components";
 
-// 회원가입인지 로그인인지 구분하기 위해 mode 를 props 로 받습니다.
-// onSubmit 도 회원가입과 로그인 페이지에서 각각 구현을 하고 props 로 넘겨줄 겁니다.
-const AuthForm = ({ mode, onSubmit }) => {
-  
-  // 무엇을 formData 에 넣어야 할까요?
-  const [formData, setFormData] = useState({
-		
-  });
+const Login = () => {
+  const { setUser } = useContext(MyContext);
 
-  const handleChange = (e) => {
-
+  const navigate = useNavigate();
+  const handleLogin = async (formData) => {
+    
+    try {
+      const data = await login(formData);
+      if (data.success) {
+        console.log("test");
+        setUser(data.user);
+        localStorage.setItem("user", JSON.stringify(data.user));
+        navigate("/");
+      } else {
+        alert("로그인에 실패했습니다.");
+      }
+    } catch (error) {
+      alert("로그인 중 문제가 발생했습니다.");
+    }
   };
 
-  const handleSubmit = (e) => {
-
-  };
-
-  // id 입력을 위한 input 만 힌트로 만들어 두었습니다. 참고해서 한번 만들어봅시다! 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="id"
-        value={formData.id}
-        onChange={handleChange}
-        placeholder="아이디"
-        required
-      />
-      <input
+    <LoginContainer>
+      <LoginTitle>로그인</LoginTitle>
+      <AuthForm mode="login" onSubmit={handleLogin} />
 
-      />
-      {mode === "signup" && (
-        <input
-          type="text"
-          name="nickname"
-          value={formData.nickname}
-          onChange={handleChange}
-          placeholder="닉네임"
-          required
-          className="w-full p-4 border border-gray-300 rounded-lg"
-        />
-      )}
-      <button type='/login'>
-        {mode === "login" ? "로그인" : "회원가입"}
-      </button>
-    </form>
+      <SignupText>계정이 없으신가요?</SignupText>
+      <LinkToSignup to="/signup">회원가입</LinkToSignup>
+    </LoginContainer>
   );
 };
 
-export default AuthForm;
+export default Login;
+
+const LoginContainer = styled.div`
+  width: 30%;
+  max-width: 500px;
+  height: 540px;
+  margin: 0 auto;
+  padding: 60px 50px;
+
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  background-color: #fff;
+  border-radius: 20px;
+  box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.1);
+
+  text-align: center;
+`;
+
+const LoginTitle = styled.h2`
+  text-align: left;
+  font-size: 1.8rem;
+  color: #333;
+  margin-bottom: 20px;
+`;
+
+const SignupText = styled.span`
+  margin-bottom: 10px;
+  font-size: 16px;
+  color: #777;
+`;
+
+const LinkToSignup = styled(Link)`
+  color: #45acf1;
+  text-decoration: none;
+  font-weight: bold;
+  padding: 10px;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
