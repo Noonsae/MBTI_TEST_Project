1dc78c4e8025914560e300ae63c58e51ac73bce4
Noon_sae <minseock7308@gmail.com>
2024-11-27 07:04:55 +0900

    📝Signup 컴포넌트 로직작성

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,33 +1,80 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { register } from "../api/auth";
 import AuthForm from "../components/AuthForm";
-// import { register } from "../api/auth";
-import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
-const Signup = () => {
+const Signup = () => {  
   const navigate = useNavigate();
 
-  // 완성된 로직들이 아니에요! 참고만 하세요!
-  const handleSignup = async (formData) => {
-    try {
-      await register(formData);
-    } catch (error) {
-      alert("회원가입에 실패했습니다. 다시 시도해주세요.");
+const handleSignup = async (FormData) => {
+  try {
+    const data = await register(FormData);
+    console.log(data); // 서버 응답 확인
+    if (data.success) {
+      alert("회원가입이 완료되었습니다.");
+      navigate("/login");
+    } else {
+      alert("회원가입 중 문제가 발생했습니다. 다시 시도해주세요.");
     }
-  };
+  } catch (error) {
+    console.error(error); // 오류 출력
+    alert("회원가입 중 문제가 발생했습니다.");
+  }
+};
+
 
   return (
-    <div>
-      <div>
-        <h1>회원가입</h1>
-        <AuthForm mode="signup" onSubmit={handleSignup} />
-        <div>
-          <p>
-            이미 계정이 있으신가요? <Link to="/login">로그인</Link>
-          </p>
-        </div>
-      </div>
-    </div>
+    <SignupContainer>
+      <SignupTitle>회원가입</SignupTitle>
+      <AuthForm mode="signup" onSubmit={handleSignup} />
+      <LoginText>이미 계정이 있으신가요?</LoginText>
+      <LinkToLogin to="/login">로그인</LinkToLogin>
+    </SignupContainer>
   );
 };
 
 export default Signup;
+
+const SignupContainer = styled.div`
+  width: 30%;
+  max-width: 500px;
+  height: 640px;
+  margin: 0 auto;
+  padding: 60px 50px;
+
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  background-color: #fff;
+  border-radius: 20px;
+  box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.1);
+
+  text-align: center;
+`;
+
+const SignupTitle = styled.h2`
+  text-align: left;
+  font-size: 1.8rem;
+  color: #333;
+  margin: 15px 0 20px;
+`;
+
+const LoginText = styled.span`
+  margin-bottom: 10px;
+  font-size: 16px;
+  color: #777;
+`;
+
+const LinkToLogin = styled(Link)`
+  color: #45acf1;
+  text-decoration: none;
+  font-weight: bold;
+  padding: 10px;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
