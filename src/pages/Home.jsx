c0f9b7b27d4a43b3ee04163d9080b1511d06de73
Noon_sae <minseock7308@gmail.com>
2024-11-27 07:02:31 +0900

    ♻️ Home 컴포넌트 리펙토링

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,26 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Home = () => {
+
+    const items = [
+      {
+        title: "성격 유형 검사",
+        description:
+          "자신의 성격 유형을 파악하고 삶의 여러 영역에서 어떤 영향을 미치는지 알아보세요.",
+      },
+      {
+        title: "성격 유형 이해",
+        description:
+          "다른 사람들이 어떻게 행동하는지 이해하는 데 도움을 줄 수 있습니다.",
+      },
+      {
+        title: "팀 평가",
+        description:
+          "팀 내에서 자신과 동료들의 성격을 이해하고 협력할 수 있는 방법을 배워보세요.",
+      },
+    ];
+
+
   return (
     <Container>
       <Title>무료 MBTI 테스트</Title>
@@ -12,26 +32,12 @@ const Home = () => {
 
       <div>
         <List>
-          <ListItem>
-            <ListItemTitle>성격 유형 검사</ListItemTitle>
-            <ListItemDescription>
-              자신의 성격 유형을 파악하고 삶의 여러 영역에서 어떤 영향을
-              미치는지 알아보세요.
-            </ListItemDescription>
-          </ListItem>
-          <ListItem>
-            <ListItemTitle>성격 유형 이해</ListItemTitle>
-            <ListItemDescription>
-              다른 사람들이 어떻게 행동하는지 이해하는 데 도움을 줄 수 있습니다.
-            </ListItemDescription>
-          </ListItem>
-          <ListItem>
-            <ListItemTitle>팀 평가</ListItemTitle>
-            <ListItemDescription>
-              팀 내에서 자신과 동료들의 성격을 이해하고 협력할 수 있는 방법을
-              배워보세요.
-            </ListItemDescription>
-          </ListItem>
+          {items.map((item, index) => (
+            <ListItem key={index}>
+              <ListItemTitle>{item.title}</ListItemTitle>
+              <ListItemDescription>{item.description}</ListItemDescription>
+            </ListItem>
+          ))}
         </List>
       </div>
       <StyledLink to="/login">로그인하기</StyledLink>
