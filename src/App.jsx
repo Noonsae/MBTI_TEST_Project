c559be3a56449979393f493708e48ee9dfed9893
Noon_sae <minseock7308@gmail.com>
2024-11-27 06:58:10 +0900

    🔗MyProvider - App 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { GlobalStyle } from "../styles/GlobalStyled";
+import { MyProvider } from "./context/MyContext";
 import Router from "./shared/Router";
 
 const App = () => {
   return (
-    <>
+    <MyProvider>
       <GlobalStyle />
       <Router />
-    </>
+    </MyProvider>
   );
 };
 
