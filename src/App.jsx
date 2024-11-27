f6a27eecf61e6ad92d9d7a638a1c41db5d915255
Noon_sae <minseock7308@gmail.com>
2024-11-28 06:30:08 +0900

    🔗토스트 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { GlobalStyle } from "../styles/GlobalStyled";
-
+import { AuthProvider } from "./context/AuthContext";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Router from "./shared/Router";
 
 const App = () => {
+  
   return (
     <AuthProvider>
       <GlobalStyle />
       <Router />
+      <ToastContainer />
     </AuthProvider>
   );
 };
