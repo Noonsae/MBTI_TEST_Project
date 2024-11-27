36bc6852060e5f02a1734c75688244872914139c
Noon_sae <minseock7308@gmail.com>
2024-11-28 06:35:41 +0900

    🚧 TestResult 로직 작성 진행 중

--- src/pages/TestResult.jsx
+++ src/pages/TestResult.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import TestResultList from "../components/TestResultList";
+import { getTestResults } from "../api/testResults";
 
 const TestResult = () => {
+  const [testResults, setTestResults] = useState([]);
+  const [loading, setLoading] = useState(true); // 로딩 상태 추가
+
+  useEffect(() => {
+    const fetchTestResults = async () => {
+      try {
+        setLoading(true); // 로딩 시작
+        const results = await getTestResults();
+        setTestResults(results);
+      } catch (error) {
+        console.error("Error fetching test results:", error);
+      } finally {
+        setLoading(false); // 로딩 끝
+      }
+    };
+
+    fetchTestResults();
+  }, []);
+
   return (
-    <div>
-      <h1>테스트 결과 페이지</h1>
-    </div>
+    <>
+      <div>
+        <h1>모든 테스트 결과</h1>
+        {loading ? ( // 로딩 상태 표시
+          <p>로딩 중...</p>
+        ) : (
+          <TestResultList
+            testResults={testResults}
+            setTestResults={setTestResults}
+          />
+        )}
+      </div>
+    </>
   );
 };
 
