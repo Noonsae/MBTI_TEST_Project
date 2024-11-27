6759e36be198a1c3049f6d9768d670bc49485875
Noon_sae <minseock7308@gmail.com>
2024-11-28 06:45:19 +0900

    📝 props 이름 변경

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,15 +1,11 @@
 import { Navigate, Outlet } from "react-router-dom";
 
-function ProtectedRoute({ token }) {
-  if (!token) {
+function ProtectedRoute({ login }) {
+  if (!login) {
     // 로그인되지 않았다면 로그인페이지로 강제 이동
     return <Navigate to="/login" />;
   }
-  return (
-    <>      
-      <Outlet />;
-    </>
-  );
+  return <Outlet />;
 }
 
 export default ProtectedRoute;
