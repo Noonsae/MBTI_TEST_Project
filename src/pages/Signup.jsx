d90d67e75e177c5e55db54008d1ff08d83b8a1f2
Noon_sae <minseock7308@gmail.com>
2024-11-27 23:54:54 +0900

    🩹 Axios 함수 호출 함수명 변경 register → signUp

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,28 +1,29 @@
 import React, { useContext } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import { register } from "../api/auth";
+import { signUp } from "../api/auth";
 import AuthForm from "../components/AuthForm";
 import styled from "styled-components";
 
-const Signup = () => {  
+const Signup = () => {
   const navigate = useNavigate();
 
-const handleSignup = async (FormData) => {
-  try {
-    const data = await register(FormData);
-    console.log(data); // 서버 응답 확인
-    if (data.success) {
-      alert("회원가입이 완료되었습니다.");
-      navigate("/login");
-    } else {
-      alert("회원가입 중 문제가 발생했습니다. 다시 시도해주세요.");
-    }
-  } catch (error) {
-    console.error(error); // 오류 출력
-    alert("회원가입 중 문제가 발생했습니다.");
-  }
-};
+  const handleSignup = async (FormData) => {
 
+
+    try {
+      const data = await signUp(FormData);
+      console.log(data); // 서버 응답 확인
+      if (data.success) {
+        alert("회원가입이 완료되었습니다.");
+        navigate("/login");
+      } else {
+        alert("회원가입 중 문제가 발생했습니다. 다시 시도해주세요.");
+      }
+    } catch (error) {
+      console.error(error); // 오류 출력
+      alert("회원가입 중 문제가 발생했습니다.");
+    }
+  };
 
   return (
     <SignupContainer>
@@ -39,9 +40,8 @@ export default Signup;
 const SignupContainer = styled.div`
   width: 30%;
   max-width: 500px;
-  height: 640px;
   margin: 0 auto;
-  padding: 60px 50px;
+  padding: 40px 50px 60px;
 
   position: absolute;
   left: 50%;
