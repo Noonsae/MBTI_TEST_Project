bec1f91dd1db6ea7fa4b12e04de247f5e243646b
Noon_sae <minseock7308@gmail.com>
2024-11-27 23:58:13 +0900

    📝auth 일부 로직 수정 && 예외, 오류처리 로직 추가

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,31 +2,36 @@ import axios from "axios";
 
 const API_URL = "https://moneyfulpublicpolicy.co.kr";
 
-export const register = async (userData) => {
+// 회원가입 함수
+export const signUp = async (userData) => {
   try {
-    const response = await axios.post(`${API_URL}/register`, userData);
+    const response = await axios.post(`${API_URL}/signUp`, userData);
     return response.data;
   } catch (error) {
-    throw error;
+    console.error("회원가입 오류:", error);
+    throw new Error("회원가입에 실패했습니다. 다시 시도해 주세요.");
   }
 };
 
+// 로그인 함수
 export const login = async (userData) => {
   try {
     const response = await axios.post(`${API_URL}/login`, userData);
-    if (response.data.success) {
-      localStorage.setItem(response.data.accessToken);
-      return response.data;
-    }
     return response.data;
   } catch (error) {
-    throw error;
+    console.error("로그인 오류:", error);
+    throw new Error(
+      "로그인에 실패했습니다. 아이디와 비밀번호를 확인해 주세요."
+    );
   }
 };
 
+// 사용자 프로필 가져오기 함수
 export const getUserProfile = async () => {
   const token = localStorage.getItem("accessToken");
-  if (!token) throw new Error("로그인을 먼저 해주세요.");
+  if (!token) {
+    throw new Error("로그인을 먼저 해주세요.");
+  }
 
   try {
     const response = await axios.get(`${API_URL}/user`, {
@@ -36,13 +41,24 @@ export const getUserProfile = async () => {
     });
     return response.data;
   } catch (error) {
-    throw error;
+    console.error("프로필 가져오기 오류:", error);
+    if (error.response && error.response.status === 401) {
+      throw new Error("인증에 실패했습니다. 다시 로그인해 주세요.");
+    } else {
+      throw new Error(
+        "프로필 정보를 가져오는 데 실패했습니다. 다시 시도해 주세요."
+      );
+    }
   }
 };
 
+// 프로필 업데이트 함수
 export const updateProfile = async (formData) => {
   const token = localStorage.getItem("accessToken");
-  if (!token) throw new Error("로그인을 먼저 해주세요.");
+  if (!token) {
+    throw new Error("로그인을 먼저 해주세요.");
+  }
+
   try {
     const response = await axios.patch(`${API_URL}/profile`, formData, {
       headers: {
@@ -52,6 +68,11 @@ export const updateProfile = async (formData) => {
     });
     return response.data;
   } catch (error) {
-    throw error;
+    console.error("프로필 업데이트 오류:", error);
+    if (error.response && error.response.status === 401) {
+      throw new Error("인증에 실패했습니다. 다시 로그인해 주세요.");
+    } else {
+      throw new Error("프로필 업데이트에 실패했습니다. 다시 시도해 주세요.");
+    }
   }
 };
