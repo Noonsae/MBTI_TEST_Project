bd66ddb8eff719c3689d8c2acdb3afd6adb1a7ab
Noon_sae <minseock7308@gmail.com>
2024-11-28 06:34:53 +0900

    🐛 새로고침 시 로그아웃되는 상황 해결!

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -7,22 +7,29 @@ import Test from "../pages/Test";
 import TestResult from "../pages/TestResult";
 import Layout from "../components/Layout";
 import ProtectedRoute from "../components/ProtectedRoute";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 const Router = () => {
-  const [user, setUer] = useState(null);
+
+  const { isLoggedIn } = useContext(AuthContext);
+
+  // 로그인 상태가 아직 결정되지 않은 경우 로딩 상태를 표시하거나 아무것도 렌더링하지 않음
+  if (isLoggedIn === undefined) {
+    return null; // 또는 로딩 컴포넌트를 반환할 수 있음
+  }
+  
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout/>}>
+        <Route element={<Layout />}>
           {/* Routs */}
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />}/>
+          <Route path="/signup" element={<Signup />} />
           {/* ProtectedRoute */}
-          
-          
-          <Route element={<ProtectedRoute user={user}  />}>
+
+          <Route element={<ProtectedRoute login={isLoggedIn} />}>
             <Route path="/profile" element={<Profile />} />
             <Route path="/test" element={<Test />} />
             <Route path="/result" element={<TestResult />} />
